d52791b04de45a4ee103629bf41b361ad46490d9
Kaiobaa <52858057+Kaiobaa@users.noreply.github.com>
2020-07-21 05:51:01 -0300

    Create adicao.c

---
+++ adicao.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void main()
+{
+	/* code */
+	float a;
+	printf("\nDigite a primeira parcela: ");
+	scanf("%f", &a);
+
+	float b;
+	printf("\nDigite a segunda parcela: ");
+	scanf("%f", &b);
+
+	//float c;
+	//c = a+b;
+	printf("O total eh: %f\n", a+b); // poderia ser o a variavel c tbm
+
+}
